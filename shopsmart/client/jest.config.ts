30d2ba4e40954e093b4e4c6ca715dd110bcfc832
Praneeth <iampraneeth1116@gmail.com>
2026-03-18 20:35:00 +0530

    enhanced tests

--- shopsmart/client/jest.config.ts
+++ shopsmart/client/jest.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
     coverageProvider: 'v8',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/e2e/'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/$1',
     },
